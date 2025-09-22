a51bbe9942e63195fbec39984acf13aa5e319953
rashmi-ranjan-sahoo-dev <rinkuasahoo04@gmail.com>
2025-09-22 23:19:30 +0530

    sift body

--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -2,7 +2,7 @@ import { useContext } from 'react'
 import Header from '../components/Header/Header'
 import { AuthContext } from '../components/ContextAPI/AuthContext'
 import { Outlet } from 'react-router'
-import Body from '../components/Body/Body'
+import Body from './Body/Body'
 import Footer from '../components/Footer/Footer'
 
 
